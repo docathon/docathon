89c7e0f68f66105b8449ea5ff67516448a8ab67d
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-02-26 17:42:11 -0800

    again, small fixes

--- src/watchtower/add_images_to_md.py
+++ src/watchtower/add_images_to_md.py
@@ -6,14 +6,16 @@ informations = pd.read_csv(".downloaded_projects").values
 path_root = op.abspath('.')
 exceptions = []
 for user, project in tqdm(informations):
+    filename = project.replace(" ", "_").lower()
     try:
-        with open('build/{}.md'.format(project), 'a') as ff:
+        with open('build/{}.md'.format(filename), 'a') as ff:
             path_img_read = 'build/images/{}.png'.format(project)
             if not op.exists(path_img_read):
                 print('Skipping {}'.format(project))
                 continue
             path_img_write = 'images/{}.png'.format(project)
-            ff.writelines('\n\n# Activity\n---\n![]({})'.format(path_img_write))
+            ff.writelines(
+                '\n\n# Activity\n---\n![]({})'.format(path_img_write))
     except:
         exceptions.append(project)
 print('Finished inserting images.\nExceptions: {}'.format(exceptions))
