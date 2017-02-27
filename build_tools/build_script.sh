e3d0005558e74be04e790a6d0400f8aed8119994
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-02-26 17:52:46 -0800

    plotting commits before fetching themvim ../../build_tools/build_script.sh

--- build_tools/build_script.sh
+++ build_tools/build_script.sh
@@ -6,8 +6,8 @@ echo "Fetch github commits for projects"
 pushd src/watchtower
 python update_project_questionnaire.py
 python extract_project_information.py ../data/docathon_project_registration.csv
-python plot_commits.py 
 python fetch_project_commits.py ../watchtower/.downloaded_projects
+python plot_commits.py 
 python add_images_to_md.py
 python move_project_pages.py
 popd
