6d8712da43b4bcb91236e19bab00b5f856d8ecbd
Chris Holdgraf <choldgraf@berkeley.edu>
2017-02-27 17:25:48 -0800

    making a grid layout

--- src/watchtower/create_projects_summary.py
+++ src/watchtower/create_projects_summary.py
@@ -17,8 +17,11 @@ header_index = (
     "\n"
     "# Projects\n"
     "\n "
-    "Here is a list of projects involved in the Docathon. "
-    "To add a project here, please fill in [this "
+    "Here is a list of projects involved in the Docathon along "
+    "with information about contributions to their documentation. If "
+    "you'd like to help out with a project, click an image below "
+    "to see more information about it. "
+    "If you'd like to see your project here, please fill in [this "
     "registration form](https://goo.gl/forms/0cPpw01zehrEyDDE3) \n"
     "\n")
 
@@ -30,7 +33,7 @@ sorted_totals = commit_totals.groupby('project').sum()['Doc'].sort_values(ascend
 # Create a summary page
 filename = os.path.join('build', "projects.md")
 header_formatted = header_index.format(now=date.today().strftime("%Y-%m-%d"))
-project_template = "[{project_image}]({project_url})\n\n"
+project_template = "<a href='{project_url}'><img src='{project_image}' style='width: 48%; float:left; box-shadow: none; margin: auto' /></a>"
 with open(filename, "w") as f:
     f.write(header_formatted)
     for project_name, _ in sorted_totals.items():
@@ -39,6 +42,6 @@ with open(filename, "w") as f:
             print('Skipping {}'.format(project_name))
             continue
         path_img_write = 'images/{}.png'.format(project_name)
-        project_image = '![]({})\n'.format(path_img_write)
+        project_image = path_img_write
         f.write(project_template.format(project_image=project_image,
                 project_url=project_name.lower()+".html"))
